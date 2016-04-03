843460dda8c042faa2995aace8010e1d7c49f759
Tom Imrei <tom@balr.com>
2016-04-03 15:16:04 +0200

    Update source QA tools; simplify the script running them

--- bin/runqatools.sh
+++ bin/runqatools.sh
@@ -2,58 +2,52 @@
 
 PARENT=`dirname $0`/..
 
+cd $PARENT
+
 # stop on any errors
 set -e
 
 echo Running unit tests
-${PARENT}/vendor/bin/phpunit -c ${PARENT}/app/build/phpunit.xml
+bin/phpunit
 
-${PARENT}/vendor/bin/phpcs -v\
-  --standard=PSR1,PSR2\
-  --error-severity=1\
-  --warning-severity=0\
-  --report=full\
-  --report-file=${PARENT}/app/build/phpcs/report.txt\
-  --report-width=120\
-  src
+bin/phpcs -v
 
 echo ''
 
 echo Running PHP Mess Detector
-${PARENT}/vendor/bin/phpmd\
-  src/ text\
-  codesize,controversial,design,naming,unusedcode
+bin/phpmd src/ text codesize,controversial,design,naming,unusedcode
 
 echo ''
 
 echo Running PHP Copy-Paste Detector
-${PARENT}/vendor/bin/phpcpd\
-  --exclude vendor\
-  --exclude build\
-  --log-pmd=${PARENT}/app/build/phpcpd/results.xml\
-  --min-lines=5\
-  --min-tokens=50\
-  --no-ansi\
-  --no-interaction\
+vendor/bin/phpcpd \
+  --exclude vendor \
+  --exclude build \
+  --log-pmd=./app/build/phpcpd/results.xml \
+  --min-lines=5 \
+  --min-tokens=50 \
+  --no-ansi \
+  --no-interaction \
   src
 
 echo ''
 
 echo Running PDepend
-${PARENT}/vendor/bin/pdepend\
-  --summary-xml=${PARENT}/app/build/pdepend/summary.xml\
-  --jdepend-chart=${PARENT}/app/build/pdepend/jdepend.svg\
-  --jdepend-xml=${PARENT}/app/build/pdepend/jdepend.xml\
-  --overview-pyramid=${PARENT}/app/build/pdepend/pyramid.svg\
-  --ignore=AddRest/Tests src
+bin/pdepend \
+  --summary-xml=./app/build/pdepend/summary.xml \
+  --jdepend-chart=./app/build/pdepend/jdepend.svg \
+  --jdepend-xml=./app/build/pdepend/jdepend.xml \
+  --overview-pyramid=./app/build/pdepend/pyramid.svg \
+  --ignore=Tests \
+  src
 
 echo ''
 
 echo Compiling code metrics
-${PARENT}/vendor/bin/phploc\
-  --exclude vendor --exclude ${PARENT}/build\
+bin/phploc\
+  --exclude vendor --exclude ./app/build\
   --count-tests\
-  --log-xml ${PARENT}/app/build/phploc/result.xml\
+  --log-xml ./app/build/phploc/result.xml\
   .
 
 echo ''
